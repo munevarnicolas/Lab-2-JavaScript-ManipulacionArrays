05b94cc098ff687599e9daa9d56e6a77e965c93d
Nicolas Munevar <72704297+munevarnicolas@users.noreply.github.com>
2025-11-01 22:16:45 -0500

    Add files via upload

---
+++ Ejer1-1.js
@@ -0,0 +1,15 @@
+//Ejer 1-1
+
+let nombre = "Juan";
+let edad = 25;
+let estudiante = true;
+
+
+console.log(nombre);        
+console.log(edad);          
+console.log(estudiante);  
+
+
+console.log(typeof nombre);       
+console.log(typeof edad);         
+console.log(typeof estudiante); 
